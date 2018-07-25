931c3f51b9e87e781327f18db05187396aa37235
sasatou <sasaki99999@gmail.com>
2018-07-25 23:31:09 +0900

    12日目
    整理

---
+++ slideshow/symbol.js
@@ -0,0 +1,7 @@
+// symbol
+let sym1 = Symbol('foo');
+let sym2 = Symbol('foo');
+
+console.log(typeof sym1); // symbol
+console.log(sym1.toString()); // Symbol(foo)
+console.log(sym1 === sym2); // false
